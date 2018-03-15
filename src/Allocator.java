233b31f2144dceac8c774bb433513c303391f565
kiababashahi <kiababashahi@gmail.com>
2018-03-15 14:28:03 -0400

    update

---
+++ src/Allocator.java
@@ -0,0 +1,26 @@
+
+public class Allocator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReadData rd=new ReadData();
+		rd.readFile("e05200.txt");
+		for(int i=0;i<rd.num_agents;i++) {
+			for(int j=0;j<rd.num_resources;j++) {
+				System.out.print(rd.c[i][j] +" ");
+			}
+			System.out.println();
+		}
+		System.out.println("*************");
+		for(int i=0;i<rd.num_agents;i++) {
+			for(int j=0;j<rd.num_resources;j++) {
+				System.out.print(rd.a[i][j] +" ");
+			}
+			System.out.println();
+		}
+		System.out.println("*****");
+		for(int i=0;i<rd.num_agents;i++) {
+			System.out.print(rd.b[i]+ " ");
+		}
+	}
+}
