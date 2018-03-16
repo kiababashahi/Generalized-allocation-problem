8f723fa954a9cd6c09704e05ca1d4bc1ecd1079a
kiababashahi <kiababashahi@gmail.com>
2018-03-16 19:02:01 -0400

    Generated a Feasible solution with the constructive heuristic The techinique is to allocate the item with the max(min(a[ij]))

--- src/Allocator.java
+++ src/Allocator.java
@@ -4,7 +4,7 @@ public class Allocator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ReadData rd=new ReadData();
-		rd.readFile("e05200.txt");
+		rd.readFile("e05100.txt");
 		/*for(int i=0;i<rd.num_agents;i++) {
 			for(int j=0;j<rd.num_resources;j++) {
 				System.out.print(rd.c[i][j] +" ");
